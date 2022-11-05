928cce66204b7c39a91c3b008f756272463539c4
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-11-05 19:17:46 +0200

    first versin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+function useJsonFetch(url) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          setError(response.status)
+          return response.json()
+        }
+
+        return response.json()
+      })
+      .then(json => {
+        setData(json);
+        setLoading(false);
+      })
+  }, [])
+
+  return [
+    data ? data.status : '',
+    error,
+    loading,
+  ]
+  
+}
+
+
+function Data() {
+
+  const [ data, error, loading ] = useJsonFetch('http://localhost:7070/data');
+
+  return (
+    <div className='data'>{ `${data} ${error} ${loading}` }</div>
+  )
+}
+
+function Error() {
+  const [ data, error, loading ] = useJsonFetch('http://localhost:7070/error');
+
+  return (
+    <div className='error'>{ `${data} ${error} ${loading}` }</div>
+  )
+}
+
+function Loading() {
+  const [ loading, data, error ] = useJsonFetch('http://localhost:7070/loading');
+
+  return (
+    <div className='loading'>{ `${data} ${error} ${loading}` }</div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Data />
+      <Error />
+      <Loading />
     </div>
   );
 }
